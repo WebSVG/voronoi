8d14edab87dfa71cb4ca1495e1536a1243325450
wassfila <wassim.filali@gmail.com>
2020-04-09 10:45:26 +0200

    renamed cols in stead of array

--- src/main.js
+++ src/main.js
@@ -8,53 +8,52 @@ let bs = new Bootstrap()
 
 function main(){
 
-    let seeds_cols = bs.cols(b,4,["col-4","col","col-3","col-1"])
-    //html(seeds_cols[0],"a",/*html*/`<h4 style="margin:10px">Distance</h4>`)
-    let toggle_walls = bs.toggle(seeds_cols[0],"walls away","walls stick")
-    let toggle_alg = bs.toggle(seeds_cols[0],"Sampling","Single")
-    let in_sampling = bs.input_text(seeds_cols[0],"in_nb_samples",`${vor.nb_samples} samples`);
+    let [col0,col1,col2,col3] = bs.cols(b,4,["col-4","col","col-3","col-1"])
 
-    html(seeds_cols[0],"a",/*html*/`<a style="margin:10px">Edges thickness</a>`)
-    let rg_path_width = bs.input_range(seeds_cols[0],30)
+    //col0
+    let toggle_walls = bs.toggle(col0,"walls away","walls stick")
+    let toggle_alg   = bs.toggle(col0,"Sampling","Single")
+    let in_sampling  = bs.input_text(col0,"in_nb_samples",`${vor.nb_samples} samples`);
+    html(col0,"a",/*html*/`<a style="margin:10px">Edges thickness</a>`)
+    let rg_path_width = bs.input_range(col0,30)
     rg_path_width.value = vor.path_width
-    html(seeds_cols[0],"a",/*html*/`<a style="margin:10px">min edge cells (still buggy)</a>`)
-    let rg_min_edge = bs.input_range(seeds_cols[0],50)
+    html(col0,"a",/*html*/`<a style="margin:10px">min edge cells (still buggy)</a>`)
+    let rg_min_edge = bs.input_range(col0,50)
 
-    //br(seeds_cols[0])
-    let btn_seeds = bs.button(seeds_cols[1],"btn_seed",`generate seeds`);
-    //let toggle_seeds = toggle(seeds_cols[1],"visible","hidden")
-
+    //col1
+    let btn_seeds = bs.button(col1,"btn_seed",`generate seeds`);
     const view_states = [vor.view_svg.cells,vor.view_svg.edges,vor.view_svg.seeds]
-    bs.checkbox_group(seeds_cols[1],"cbx_view",["cells","edges","seeds"],view_states,(e)=>{
-        vor.view_svg[e.target.getAttribute("data-name")] = e.target.checked
-        vor.draw()
-    })
-    //html(seeds_cols[1],"a",/*html*/`<a style="margin:10px">runtime ms</a>`)
+    bs.checkbox_group(col1,"cbx_view",["cells","edges","seeds"],view_states,(e)=>{
+                            vor.view_svg[e.target.getAttribute("data-name")] = e.target.checked
+                            vor.draw()
+                        })
 
-    let in_nb_seeds = bs.input_text(seeds_cols[2],"in_nb_seed",`${vor.nb_seeds} seeds`,"w-100");
-    //html(seeds_cols[2],"a",/*html*/`<a style="margin:10px">number of seeds</a>`)
-    let rg_nb_seeds = bs.input_range(seeds_cols[2],vor.nb_seeds * 2)
+    //col2
+    let in_nb_seeds = bs.input_text(col2,"in_nb_seed",`${vor.nb_seeds} seeds`,"w-100");
+    //html(col2,"a",/*html*/`<a style="margin:10px">number of seeds</a>`)
+    let rg_nb_seeds = bs.input_range(col2,vor.nb_seeds * 2)
     rg_nb_seeds.value = vor.nb_seeds
-    let in_max_seeds = bs.input_text(seeds_cols[2],"in_max_seed",`max seeds ${vor.nb_seeds*2}`,"w-100");
+    let in_max_seeds = bs.input_text(col2,"in_max_seed",`max seeds ${vor.nb_seeds*2}`,"w-100");
 
+    //col3
     vor.mouse_action = "move"
-    bs.radio_group(seeds_cols[3],["add","move","remove"],1,(e)=>{
-        vor.mouse_action = e.target.getAttribute("data-action")
-    })
+    bs.radio_group(col3,["add","move","remove"],1,(e)=>{
+                        vor.mouse_action = e.target.getAttribute("data-action")
+                    })
 
+    //bottom
     hr(b)
-
-    let exp_cols = bs.cols(b,3,["col-2","col-2","col"])
-    let btn_save_svg = bs.button(exp_cols[0],"btn_save",`export SVG`);
+    let [ecol0,ecol1,ecol2] = bs.cols(b,3,["col-2","col-2","col"])
+    let btn_save_svg = bs.button(ecol0,"btn_save",`export SVG`);
     const export_states = [vor.export_svg.cells,vor.export_svg.edges,vor.export_svg.seeds]
-    bs.checkbox_group(exp_cols[1],"cbx_export",["cells","edges","seeds"],export_states,(e)=>{
-        vor.export_svg[e.target.getAttribute("data-name")] = e.target.checked
-    })
+    bs.checkbox_group(ecol1,"cbx_export",["cells","edges","seeds"],export_states,(e)=>{
+                            vor.export_svg[e.target.getAttribute("data-name")] = e.target.checked
+                        })
+    let btn_save_data = bs.button(ecol2,"btn_save",`export seeds coordinates`);
+    html(ecol2,"h4",/*html*/`<a style="margin:10px">Drag and drop 'seeds.json' to import</a>`)
 
-    let btn_save_data = bs.button(exp_cols[2],"btn_save",`export seeds coordinates`);
-    html(exp_cols[2],"h4",/*html*/`<a style="margin:10px">Drag and drop 'seeds.json' to import</a>`)
 
-
+    //logic
     $(btn_save_svg).click(()=>{
         vor.save_svg("voronoi_svg_export.svg")
     })
